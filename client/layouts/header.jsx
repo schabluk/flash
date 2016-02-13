ac6830bddcaefdc12ad2631119ec96d07bbf9c5f
Łukasz Schabek <lukasz.schabek@gmail.com>
2016-02-13 21:12:05 +0100

    clean

--- client/layouts/header.jsx
+++ client/layouts/header.jsx
@@ -4,31 +4,26 @@ let isActive = (routeName) => (
   (FlowRouter.getRouteName() === routeName) ? 'active' : ''
 )
 
-const Header = () => {
-
-  console.log(_.functions(FlowRouter))
-  console.log(FlowRouter.getRouteName())
-  return (
-    <div className="pos-f-t">
-      <nav className="navbar navbar-light navbar-static-top bg-faded">
-        <div className="container">
-          <button className="navbar-toggler hidden-sm-up" type="button" data-toggle="collapse" data-target="#exCollapsingNavbar2">
-            &#9776;
-          </button>
-          <div className="collapse navbar-toggleable-xs" id="exCollapsingNavbar2">
-            <ul className="nav navbar-nav">
-              <li className={"nav-item " + isActive('main')}>
-                <a className="nav-link" href="/"><i className="fa fa-home"></i> Main</a>
-              </li>
-              <li className={"nav-item " + isActive('people')}>
-                <a className="nav-link" href="/people"><i className="fa fa-users"></i> People</a>
-              </li>
-            </ul>
-          </div>
+const Header = () => (
+  <div className="pos-f-t">
+    <nav className="navbar navbar-light navbar-static-top bg-faded">
+      <div className="container">
+        <button className="navbar-toggler hidden-sm-up" type="button" data-toggle="collapse" data-target="#exCollapsingNavbar2">
+          &#9776;
+        </button>
+        <div className="collapse navbar-toggleable-xs" id="exCollapsingNavbar2">
+          <ul className="nav navbar-nav">
+            <li className={"nav-item " + isActive('main')}>
+              <a className="nav-link" href="/"><i className="fa fa-home"></i> Main</a>
+            </li>
+            <li className={"nav-item " + isActive('people')}>
+              <a className="nav-link" href="/people"><i className="fa fa-users"></i> People</a>
+            </li>
+          </ul>
         </div>
-      </nav>
-    </div>
-  )
-}
+      </div>
+    </nav>
+  </div>
+)
 
 export default Header
